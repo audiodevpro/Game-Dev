23213274c40dae65786cee4f6ffbf5001b87287d
ts_code <ts_code@proton.me>
2022-09-28 13:06:58 -0700

    Create enemies

--- space-shooter-vite/main.js
+++ space-shooter-vite/main.js
@@ -46,6 +46,30 @@ class Projectile {
   }
 }
 
+//create enemy class
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
 //center player in the middle
 const x = canvas.width / 2
 const y = canvas.height / 2
@@ -54,6 +78,35 @@ const y = canvas.height / 2
 const player = new Player(x, y, 30, 'blue')
 
 const projectiles = []
+const enemies = []
+
+//create enemies
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4
+
+    let x
+    let y
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
+
+    const color = 'green'
+
+    //push enemies toward the center
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle)
+    }
+    enemies.push(new Enemy(x, y, radius, color, velocity))
+  }, 1000)
+}
 
 //create an animation function
 function animate() {
@@ -62,6 +115,10 @@ function animate() {
   player.draw()
   projectiles.forEach((projectile) => {
     projectile.update()
+  })
+
+  enemies.forEach((enemy) => {
+    enemy.update()
   })
 }
 
@@ -82,3 +139,4 @@ addEventListener('click', (e) => {
 
 //Call the animation function
 animate()
+spawnEnemies()
