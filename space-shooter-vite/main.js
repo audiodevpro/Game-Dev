b528b74c2fc63f2386c18147a95e3cd5c74722d7
ts_code <ts_code@proton.me>
2022-09-28 15:06:18 -0700

    Create particle explosion on hit

--- space-shooter-vite/main.js
+++ space-shooter-vite/main.js
@@ -69,6 +69,36 @@ class Enemy {
     this.y = this.y + this.velocity.y
   }
 }
+const friction = 0.99
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+    this.alpha = 1
+  }
+
+  draw() {
+    c.save()
+    c.globalAlpha = this.alpha
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.restore()
+  }
+
+  update() {
+    this.draw()
+    this.velocity.x *= friction
+    this.velocity.y *= friction
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+    this.alpha -= 0.01
+  }
+}
 
 //center player in the middle
 const x = canvas.width / 2
@@ -79,6 +109,7 @@ const player = new Player(x, y, 10, 'white')
 
 const projectiles = []
 const enemies = []
+const particles = []
 
 //create enemies
 function spawnEnemies() {
@@ -110,17 +141,24 @@ function spawnEnemies() {
 }
 
 //create an animation function
-
 let animationId
 function animate() {
   animationId = requestAnimationFrame(animate)
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.draw()
-  projectiles.forEach((projectile) => {
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1)
+    } else {
+      particle.update()
+    }
+  })
+
+  projectiles.forEach((projectile, index) => {
     projectile.update()
 
-    // remove from edges of screen
+    //remove from edges of screen
     if (
       projectile.x + projectile.radius < 0 ||
       projectile.x - projectile.radius > canvas.width ||
@@ -147,6 +185,21 @@ function animate() {
 
       //when projectiles touch enemy
       if (dist - enemy.radius - projectile.radius < 1) {
+        // create explosions
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 6),
+                y: (Math.random() - 0.5) * (Math.random() * 6)
+              }
+            )
+          )
+        }
         if (enemy.radius - 10 > 5) {
           gsap.to(enemy, {
             radius: enemy.radius - 10
@@ -180,6 +233,6 @@ addEventListener('click', (e) => {
   )
 })
 
-//Call the animation function
+//call the animation function
 animate()
 spawnEnemies()
