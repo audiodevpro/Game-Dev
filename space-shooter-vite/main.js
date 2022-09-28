9e29303964ea748f6e0c5b2f751fcd5df2435089
ts_code <ts_code@proton.me>
2022-09-28 13:57:49 -0700

    Colorized game and added 'end game' functionality

--- space-shooter-vite/main.js
+++ space-shooter-vite/main.js
@@ -75,7 +75,7 @@ const x = canvas.width / 2
 const y = canvas.height / 2
 
 //define new player and draw it on the screen
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, 'white')
 
 const projectiles = []
 const enemies = []
@@ -96,7 +96,8 @@ function spawnEnemies() {
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
     }
 
-    const color = 'green'
+    //randomize enemy color
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`
 
     //push enemies toward the center
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
@@ -109,16 +110,37 @@ function spawnEnemies() {
 }
 
 //create an animation function
+
+let animationId
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+  animationId = requestAnimationFrame(animate)
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
   player.draw()
   projectiles.forEach((projectile) => {
     projectile.update()
+
+    // remove from edges of screen
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(projectileIndex, 1)
+      }, 0)
+    }
   })
 
   enemies.forEach((enemy, index) => {
     enemy.update()
+
+    //end game when enemy collides with player
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+    if (dist - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId)
+    }
 
     projectiles.forEach((projectile, projectileIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
@@ -141,11 +163,11 @@ addEventListener('click', (e) => {
     e.clientX - canvas.width / 2
   )
   const velocity = {
-    x: Math.cos(angle),
-    y: Math.sin(angle)
+    x: Math.cos(angle) * 5,
+    y: Math.sin(angle) * 5
   }
   projectiles.push(
-    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
   )
 })
 
